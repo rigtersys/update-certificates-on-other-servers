96184c986e10ccdaa5ec445818471977c288b3f1
Robin van Leeuwen <rvl@rigter.com>
2017-04-04 15:23:34 +0200

    Check if script is run by root

--- update-certificates.sh
+++ update-certificates.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+
 function usage {
 
    echo "Usage: <domain> <servertype>"
