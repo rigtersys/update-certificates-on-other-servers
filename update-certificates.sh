d1431c67d70b131e22118ecb9a0f0a4f4b648fac
Robin van Leeuwen <robinvanleeuwen@gmail.com>
2018-06-07 11:26:53 +0200

    Update for imap and smtp

--- update-certificates.sh
+++ update-certificates.sh
@@ -9,19 +9,24 @@ fi
 
 
 function usage {
-
+   echo
    echo "Usage: <domain> <servertype>"
    echo ""
    echo "Where <servertype> is one of the following:"
    echo "- dovecot"
    echo "- postfix"
+   echo 
+   echo "Example: $0 smtp.rigter.com postfix"
+   echo "         $0 imap.rigter.com dovecot"
+   echo
+   exit
 
 }
 
 
 function get_certs {
 	echo "- Creating new certificates on firewall for domain $1"
-	ssh root@firewall1 "cd /opt/dehydrated; ./renew_single_certificate.sh $1"		
+	ssh root@firewall1 "cd /opt/dehydrated; ./renew_single_certificate.sh $1"
 
 	echo "Retrieving certificates from firewall"
 	rsync -a -e "ssh root@firewall1" :/opt/dehydrated/certs/$1/*-*.pem /etc/letsencrypt/archive/$1/
@@ -31,7 +36,7 @@ function get_certs {
 	rm /etc/letsencrypt/live/$1/*
 	echo "- Creating relevant symlinks for the new certificates"
 	cd /etc/letsencrypt/live/$1
-	for f in privkey fullchain chain cert; do 
+	for f in privkey fullchain chain cert; do
 	    ls -1tr ../../archive/$1/$f-*.pem| tail -n 1 | while read i; do ln -s $i $f.pem; done;
         done;
 }
@@ -49,4 +54,3 @@ if [[ "$2" =~ ^(dovecot|postfix)$ ]]; then
 else
     usage
 fi
-
